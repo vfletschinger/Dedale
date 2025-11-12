c86ae1cbdb2df756ec9354310d2c318062a60082
Simon <simon.williams@etu.unistra.fr>
2025-11-12 15:08:36 +0100

    fit : Working basic nav

--- Frontend/Mobile/Dedale/src/screens/Home.tsx
+++ Frontend/Mobile/Dedale/src/screens/Home.tsx
@@ -1,17 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+
+import "../style/global.css";
+
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Dedale</Text>
+    <View className="container">
+      <Text className="text-xl font-bold text-blue-500">Welcome to Dedale</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
