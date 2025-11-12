93d45a8e6f8c44ec835f3379bd7b9162915c4d0c
Nikita <nikita.mag.0027@gmail.com>
2025-11-12 13:45:28 +0100

    [feat web] nav cachée sur la page du login

--- Frontend/web/app/root.tsx
+++ Frontend/web/app/root.tsx
@@ -6,6 +6,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
 } from "react-router";
 
 import type { Route } from "./+types/root";
@@ -13,8 +14,9 @@ import "./app.css";
 import { Button } from "./components/ui/button";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const hideNav = location.pathname === "/";
   return (
-
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
@@ -24,12 +26,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <div className="min-h-screen flex flex-col">
-          <nav className="bg-slate-600 text-white p-4 space-x-2">
-            <Button asChild variant="link" className="text-secondary"><NavLink to="/accueil">Accueil</NavLink></ Button>
-            <Button asChild variant="link" className="text-secondary"><NavLink to="/carte" prefetch="intent">Carte</NavLink></Button>
-            <Button asChild variant="link" className="text-secondary"><NavLink to="/equipes" prefetch="intent">Equipes</NavLink></Button>
-            <Button asChild variant="link" className="text-secondary"><NavLink to="/planning" prefetch="intent">Planning</NavLink></Button>
-          </nav>
+          {hideNav ? "" : (
+            <nav className="bg-slate-600 text-white p-4 space-x-2">
+              <Button asChild variant="link" className="text-secondary"><NavLink to="/accueil">Accueil</NavLink></Button>
+              <Button asChild variant="link" className="text-secondary"><NavLink to="/carte" prefetch="intent">Carte</NavLink></Button>
+              <Button asChild variant="link" className="text-secondary"><NavLink to="/equipes" prefetch="intent">Équipes</NavLink></Button>
+              <Button asChild variant="link" className="text-secondary"><NavLink to="/planning" prefetch="intent">Planning</NavLink></Button>
+            </nav>
+          )}
           <main className="flex-1 flex flex-row overflow-y-scroll">
             {children}
           </main>
