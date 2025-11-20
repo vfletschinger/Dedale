3a2fa876368b695c9eef6ba33cbdcad6fa5580b5
val <valfletsch@gmail.com>
2025-11-20 14:16:44 +0100

    [fix web] clean code

--- Frontend/Web/Dedale/src-tauri/src/lib.rs
+++ Frontend/Web/Dedale/src-tauri/src/lib.rs
@@ -3,6 +3,7 @@
 mod db;
 mod excel;
 mod pdf;
+mod map;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -14,14 +15,8 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             excel::export_points_excel,
             pdf::create_pdf,
-            get_points
+            map::get_points
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-async fn get_points(app: tauri::AppHandle) -> Result<serde_json::Value, String> {
-    let pts = crate::db::retrieve_data(&app).await?;
-    serde_json::to_value(pts).map_err(|e| e.to_string())
 }
