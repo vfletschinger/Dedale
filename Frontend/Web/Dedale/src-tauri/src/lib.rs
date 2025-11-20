4f73053ee0bb2fb68dce5381f7b0664bc464b6d6
val <valfletsch@gmail.com>
2025-11-20 10:22:50 +0100

    [feat web] display points on map

--- Frontend/Web/Dedale/src-tauri/src/lib.rs
+++ Frontend/Web/Dedale/src-tauri/src/lib.rs
@@ -13,8 +13,15 @@ pub fn run() {
         .plugin(db::init_db())
         .invoke_handler(tauri::generate_handler![
             excel::export_points_excel,
-            pdf::create_pdf
+            pdf::create_pdf,
+            get_points
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn get_points(app: tauri::AppHandle) -> Result<serde_json::Value, String> {
+    let pts = crate::db::retrieve_data(&app).await?;
+    serde_json::to_value(pts).map_err(|e| e.to_string())
 }
